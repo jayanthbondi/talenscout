d1f2d890b7f53668f5e7a5d30f1d2c656a9e93a5
jayanth bondi <jayanth1209bondi@gmail.com>
2024-10-14 10:13:41 +0530

    Fix CollectionList and Sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   Box,
   List,
@@ -12,7 +12,7 @@ import {
   Search,
   Collections,
   Person,
-  Bookmark,
+  // Bookmark,
   ExitToApp,
   ChevronLeft,
   ChevronRight,
@@ -25,21 +25,18 @@ const drawerWidth = 140;
 const Sidebar = () => {
   const [open, setOpen] = useState(true);
   const theme = useTheme();
-  const location = useLocation();
 
   const toggleDrawer = () => setOpen(!open);
-
-  const isActive = (path) => location.pathname === path;
 
   return (
     <MuiDrawer
       variant="permanent"
       open={open}
       sx={{
-        width: drawerWidth,
+        width: open ? drawerWidth : `calc(${theme.spacing(7)} + 1px)`,
         flexShrink: 0,
         whiteSpace: "nowrap",
-        overflow: "hidden", // Prevent overflow issues
+        overflow: "hidden",
         "& .MuiDrawer-paper": {
           width: open ? drawerWidth : `calc(${theme.spacing(7)} + 1px)`,
           transition: theme.transitions.create("width", {
@@ -48,83 +45,107 @@ const Sidebar = () => {
               ? theme.transitions.duration.enteringScreen
               : theme.transitions.duration.leavingScreen,
           }),
-          overflow: "hidden", // Avoid scrollbar
+          overflow: "hidden",
         },
       }}
     >
-      <List>
-        {menuItems.map(({ path, icon: Icon, label }) => (
-          <ListItem
-            key={path}
-            button
-            component={Link}
-            to={path}
-            sx={{
-              display: "flex",
-              gap: "10px",
-              padding: "8px",
-              backgroundColor: isActive(path)
-                ? theme.palette.primary.light
-                : "transparent",
-              "&:hover": {
-                backgroundColor: theme.palette.primary.light,
-              },
-              color: isActive(path)
-                ? theme.palette.primary.main
-                : theme.palette.grey[700],
-              whiteSpace: "nowrap", // Prevent text overflow
-            }}
-          >
-            <ListItemIcon
-              sx={{
-                minWidth: "auto",
-                color: isActive(path)
-                  ? theme.palette.primary.main
-                  : theme.palette.grey[700],
-              }}
-            >
-              <Icon sx={{ fontSize: 30 }} />
-            </ListItemIcon>
-            {open && (
-              <ListItemText
-                primary={label}
-                sx={{
-                  color: isActive(path)
-                    ? theme.palette.primary.main
-                    : theme.palette.grey[700],
-                }}
-              />
-            )}
-          </ListItem>
-        ))}
-      </List>
-
-      {/* Spacer to push the toggle button to the bottom */}
+      <MenuItemsList open={open} />
       <Box sx={{ flexGrow: 1 }} />
-
-      {/* Toggle Drawer Button */}
-      <Box sx={{ textAlign: "center", py: 2 }}>
-        <IconButton
-          onClick={toggleDrawer}
-          sx={{
-            backgroundColor: "transparent",
-            "&:hover": {
-              backgroundColor: "transparent",
-            },
-            color: theme.palette.grey[700],
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          {open ? (
-            <ChevronLeft sx={{ fontSize: 30 }} />
-          ) : (
-            <ChevronRight sx={{ fontSize: 30 }} />
-          )}
-        </IconButton>
-      </Box>
+      <DrawerToggle open={open} toggleDrawer={toggleDrawer} />
     </MuiDrawer>
+  );
+};
+
+const MenuItemsList = ({ open }) => {
+  const location = useLocation();
+  const isActive = (path) => location.pathname === path;
+
+  return (
+    <List>
+      {menuItems.map(({ path, icon: Icon, label }) => (
+        <ListItemLink
+          key={path}
+          path={path}
+          icon={Icon}
+          label={label}
+          isActive={isActive(path)}
+          open={open}
+        />
+      ))}
+    </List>
+  );
+};
+
+const ListItemLink = ({ path, icon: Icon, label, isActive, open }) => {
+  const theme = useTheme();
+
+  return (
+    <ListItem
+      button
+      component={Link}
+      to={path}
+      sx={{
+        display: "flex",
+        gap: "10px",
+        padding: "8px",
+        backgroundColor: isActive ? theme.palette.primary.light : "transparent",
+        "&:hover": {
+          backgroundColor: theme.palette.primary.light,
+        },
+        color: isActive ? theme.palette.primary.main : theme.palette.grey[700],
+        whiteSpace: "nowrap",
+        height: "45px",
+      }}
+    >
+      <ListItemIcon
+        sx={{
+          minWidth: "auto",
+          color: isActive
+            ? theme.palette.primary.main
+            : theme.palette.grey[700],
+        }}
+      >
+        <Icon sx={{ fontSize: 30 }} />
+      </ListItemIcon>
+      {open && (
+        <ListItemText
+          primary={label}
+          sx={{
+            color: isActive
+              ? theme.palette.primary.main
+              : theme.palette.grey[700],
+          }}
+        />
+      )}
+    </ListItem>
+  );
+};
+
+const DrawerToggle = ({ open, toggleDrawer }) => {
+  const theme = useTheme();
+
+  return (
+    <Box sx={{ textAlign: "center", py: 2 }}>
+      <IconButton
+        onClick={toggleDrawer}
+        sx={{
+          backgroundColor: "transparent",
+          color: theme.palette.grey[700],
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          "&:hover": {
+            backgroundColor: "transparent",
+          },
+        }}
+      >
+        {open ? (
+          <ChevronLeft sx={{ fontSize: 30 }} />
+        ) : (
+          <ChevronRight sx={{ fontSize: 30 }} />
+        )}
+      </IconButton>
+    </Box>
   );
 };
 
@@ -132,7 +153,7 @@ const menuItems = [
   { path: "/search", icon: Search, label: "Search" },
   { path: "/collection", icon: Collections, label: "Collections" },
   { path: "/profile", icon: Person, label: "Profile" },
-  { path: "/saved-searches", icon: Bookmark, label: "Saved" },
+  // { path: "/saved-searches", icon: Bookmark, label: "Saved" },
   { path: "/logout", icon: ExitToApp, label: "Logout" },
 ];
 
